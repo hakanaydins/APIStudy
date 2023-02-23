4142ca5df89b6f55259cd7f973ebf960970b0993
hakanaydins <auratheseason@gmail.com>
2023-02-23 22:54:52 +0300

    updated 23.02.2023

---
+++ src/test/java/getRequest/Get01.java
@@ -0,0 +1,27 @@
+package getRequest;
+
+public class Get01 {
+   /*
+
+
+    */
+
+
+
+    /*
+    Given
+        https://restful-booker.herokuapp.com/booking/781
+    When
+        Kullanıcı GET Methodu ile Request Gönderir
+    Then
+        Status Code un "200" olduğunu Assert et
+    And
+        Content Type ın "application/json" olduğunu assert et
+    And
+        Status Line "HTTP/1.1 200 OK" olduğunu assert et.
+
+     */
+
+
+
+}
